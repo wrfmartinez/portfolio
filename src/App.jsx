7db7a7b8522ccf793120d23a3e89c14460e0d21c
wrfmartinez <wrmartinez709@gmail.com>
2024-05-09 15:39:19 -0400

    Create Contact component and add it to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar'
 import Header from './components/Header'
 import About from './components/About'
 import Projects from './components/Projects'
+import Contact from './components/Contact'
 
 function App() {
 
@@ -13,6 +14,7 @@ function App() {
     <Header></Header>
     <About></About>
     <Projects></Projects>
+    <Contact></Contact>
   </>
   )
 }
---
+++ src/components/Contact.jsx
@@ -0,0 +1,11 @@
+function Contact() {
+    return(
+    <>
+      <h2>Contact</h2>
+      <p>I'm always open to insights into the software industry and opportunities</p>
+      <a href='mailto=wrfmartinez@gmail.com'>Contact me</a>
+    </>
+    )
+}
+
+export default Contact
