778740539acba9ab6f233d5a77c51da1199e09e5
wrfmartinez <wrmartinez709@gmail.com>
2024-05-09 17:03:44 -0400

    Add conditional rendering to About component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,21 +1,34 @@
 import aboutData from "../data/about/aboutData"
 
 function About() {
-    const listOfSkills = aboutData.skills.map((skill, idx) =>
-      <li className="skill__item" key={idx}>{ skill }</li>
-    )
+  const listOfSkills = aboutData.skills.map((skill, idx) => {
+    if (skill.frontend) {
+      return <div>
+        <li className="about__skill-item" key={idx}>
+          { skill.toolName } <span className="about__skill-tag">#frontend</span>
+        </li>
+      </div>
+    } else if (skill.backend) {
+      return <li className="about__skill-item" key={idx}>
+        { skill.toolName } <span className="about__skill-tag">#backend</span>
+      </li>
+    } else {
+      return <li className="about__skill-item" key={idx}>
+        { skill.toolName } <span className="about__skill-tag">#other</span>
+      </li>
+    }
+})
 
-    return(
-    <>
-      <section id="about">
-        <h2 className="about__title">About</h2>
-        <p className="about__desc">{ aboutData.desc }</p>
-        <ul className="skill__list">
-          { listOfSkills }
-        </ul>
-      </section>
-    </>   
-    )
+  return(
+  <>
+    <section id="about">
+      <h2 className="about__title">About</h2>
+      <p className="about__desc">{ aboutData.desc }</p>
+      <h3>Tools I've Worked With</h3>
+      { listOfSkills }
+    </section>
+  </>   
+  )
 }
 
 export default About
