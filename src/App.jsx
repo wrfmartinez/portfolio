0036689daa0da7e6d8d52d77662081b1dd9d1cfa
wrfmartinez <wrmartinez709@gmail.com>
2024-05-09 14:33:44 -0400

    Create Projects component and add it to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import Navbar from './components/Navbar'
 import Header from './components/Header'
 import About from './components/About'
+import Projects from './components/Projects'
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
     <Navbar></Navbar>
     <Header></Header>
     <About></About>
+    <Projects></Projects>
   </>
   )
 }
---
+++ src/components/Projects.jsx
@@ -0,0 +1,15 @@
+function Projects() {
+    return(
+    <>
+      <h2>My Work</h2>
+      <ul className="project__list">
+        <li className="project__item"></li>
+        <li className="project__item"></li>
+        <li className="project__item"></li>
+        <li className="project__item"></li>
+      </ul>
+    </>    
+    )
+}
+
+export default Projects
