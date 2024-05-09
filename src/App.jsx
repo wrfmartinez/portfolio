88bbb97bf911726be031b04dd136f824db112a75
wrfmartinez <wrmartinez709@gmail.com>
2024-05-09 15:48:41 -0400

    Create Footer component and add it to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Header from './components/Header'
 import About from './components/About'
 import Projects from './components/Projects'
 import Contact from './components/Contact'
+import Footer from './components/Footer'
 
 function App() {
 
@@ -15,6 +16,7 @@ function App() {
     <About></About>
     <Projects></Projects>
     <Contact></Contact>
+    <Footer></Footer>
   </>
   )
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,27 @@
+function Footer() {
+  return(
+    <>
+      <h2 className="footer__logo">W/M</h2>
+      <ul className="footer__link-list">
+        <li className="footer__link-item">
+          <a className="footer__link" href="https://www.linkedin.com/in/wrfmartinez/" target="_blank">
+            <span className="fa-brands fa-linkedin" aria-hidden="true"></span><span className="sr-only">LinkedIn</span>
+          </a>
+        </li>
+        <li className="footer__link-item">
+          <a className="footer__link" href="https://github.com/wrfmartinez" target="_blank">
+            <span className="fa-brands fa-github" aria-hidden="true"></span><span className="sr-only">GitHub</span>
+          </a>
+        </li>
+        <li className="footer__link-item">
+          <a className="footer__link" href="mailto:wrfmartinez@gmail.com">
+            <span className="fa-solid fa-square-envelope" aria-hidden="true"></span><span className="sr-only">Email</span>
+          </a>
+        </li>
+      </ul>
+      <p className="footer__copyright-text"><small>&copy; 2024 William Martinez. All rights reserved.</small></p>
+    </>
+  )
+}
+
+export default Footer
