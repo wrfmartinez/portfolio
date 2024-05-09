69a471d01d6d0970126aae15d0c6e29376d96268
wrfmartinez <wrmartinez709@gmail.com>
2024-05-09 14:15:39 -0400

    Create Navbar component and add it to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import './App.css'
+import Navbar from './components/Navbar'
 
 function App() {
-  return <>
 
+
+  return (
+  <>
+    <Navbar></Navbar>
   </>
+  )
 }
 
 export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+function Navbar() {
+    return (
+    <>
+      <nav className="navbar">
+        <ul className="nav__list">
+          <li className="nav__item">About Me</li>
+          <li className="nav__item">My Work</li>
+          <li className="nav__item">Contact</li>
+        </ul>
+      </nav>
+    </>
+    )
+}
+
+export default Navbar
