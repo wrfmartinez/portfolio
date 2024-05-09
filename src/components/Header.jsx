6edbac6b6ca58bebef7b7a45bcfb68cac86d27bd
wrfmartinez <wrmartinez709@gmail.com>
2024-05-09 18:27:46 -0400

    Add styles to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,8 @@ function Header() {
     return (
     <>
       <header id="hero">
+        <h2>Software Engineer</h2>
+        <p>design <span>and engineer</span></p>
         <h1>William Martinez</h1>
       </header>
     </>
