d49ac343812af5ac444b9ba19a1674668b710429
wrfmartinez <wrmartinez709@gmail.com>
2024-05-09 15:12:44 -0400

    Add aboutData.js data to About component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,14 +1,16 @@
+import aboutData from "../data/about/aboutData"
+
 function About() {
+    const listOfSkills = aboutData.skills.map((skill, idx) =>
+      <li className="skill__item" key={idx}>{ skill }</li>
+    )
+
     return(
     <>
       <h2 className="about__title">About</h2>
-      <p className="about__desc">A description of who you are</p>
+      <p className="about__desc">{ aboutData.desc }</p>
       <ul className="skill__list">
-        <li className="skill__item"></li>
-        <li className="skill__item"></li>
-        <li className="skill__item"></li>
-        <li className="skill__item"></li>
-        <li className="skill__item"></li>
+        { listOfSkills }
       </ul>
     </>   
     )
---
+++ src/data/about/aboutData.js
@@ -0,0 +1,19 @@
+const aboutData = {
+    desc: 'Software Engineer with experience in software design, software testing, and frontend development with understanding of REST API, database management and server side infrastructure',
+    skills: [
+        'JavaScript',
+        'Java', 
+        'React',
+        'Node.js',
+        'MongoDB',
+        'HTML',
+        'CSS',
+        'Version Control',
+        'GitHub',
+        'Responsive Web Design',
+        'DevOps',
+        'Chrome DevTools'
+    ],
+}
+
+export default aboutData
