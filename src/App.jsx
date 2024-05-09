d2215b4dc213d7d32cb0fb1ba0f2e3ab6162cfa7
wrfmartinez <wrmartinez709@gmail.com>
2024-05-09 14:24:49 -0400

    Create About component and add it to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Navbar from './components/Navbar'
 import Header from './components/Header'
+import About from './components/About'
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
   <>
     <Navbar></Navbar>
     <Header></Header>
+    <About></About>
   </>
   )
 }
---
+++ src/components/About.jsx
@@ -0,0 +1,17 @@
+function About() {
+    return(
+    <>
+      <h2 className="about__title">About</h2>
+      <p className="about__desc">A description of who you are</p>
+      <ul className="skill__list">
+        <li className="skill__item"></li>
+        <li className="skill__item"></li>
+        <li className="skill__item"></li>
+        <li className="skill__item"></li>
+        <li className="skill__item"></li>
+      </ul>
+    </>   
+    )
+}
+
+export default About
