6d5ec2c24084ee31cfee731c92f840f520b8f774
wrfmartinez <wrmartinez709@gmail.com>
2024-05-09 14:19:17 -0400

    Create Main component and add it to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import Navbar from './components/Navbar'
+import Main from './components/Main'
 
 function App() {
 
@@ -7,6 +8,7 @@ function App() {
   return (
   <>
     <Navbar></Navbar>
+    <Main></Main>
   </>
   )
 }
---
+++ src/components/Main.jsx
@@ -0,0 +1,9 @@
+function Main() {
+    return (
+    <>
+      <h1>William Martinez</h1>
+    </>
+    )
+}
+
+export default Main
